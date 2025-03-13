ac2bbe842bf584cbf79e0db71083e30e5792e85d
Pepijn <pepijndevue@hotmail.com>
2025-03-14 00:43:59 +0100

    V1.2.0

--- src/spotify_client.py
+++ src/spotify_client.py
@@ -16,7 +16,7 @@ def load_client() -> spotipy.Spotify:
     auth_manager = SpotifyOAuth(
         client_id=st.secrets["SPOTIFY_CLIENT_ID"],
         client_secret=st.secrets["SPOTIFY_CLIENT_SECRET"],
-        redirect_uri='http://localhost:8080',
+        redirect_uri='127.0.0.1',
         scope='playlist-read-private'
     )
 

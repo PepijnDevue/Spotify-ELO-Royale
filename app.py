1cb8f70a4c3c33f1fef3e49531c4821deecbed81
Pepijn <pepijndevue@hotmail.com>
2025-03-14 18:15:07 +0100

    V1.3.5

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ gui.display_header()
 
 playlist_url, max_rounds_inputs = gui.display_sidebar()
 
-max_rounds = tournament.calc_max_rounds(max_rounds_inputs)
-
-gui.display_rounds(max_rounds)
-
 statehandler.init_session_state()
 
 if playlist_url:
     statehandler.set_session_state("tracks", spotify.get_playlist_tracks(playlist_url))
+
+    max_rounds = tournament.calc_max_rounds(max_rounds_inputs)
+    gui.display_rounds(max_rounds)
 
     if st.session_state.current_round <= max_rounds:
         
